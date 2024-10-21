11ca36d0cb7197de2b46a7a62c42ccbe1d9deb26
wa-billy <akwa2562@gmail.com>
2024-10-21 14:28:52 +0700

    Exercise5 by Nong Wa

---
+++ 5_1_Akwa_B.py
@@ -0,0 +1,22 @@
+a = int(input("Enter number :"))
+b = int(input("Enter number :"))
+c = a+b
+print(a,"+",b,"=",c)
+d = int(input("Enter number :"))
+e = int(input("Enter number :"))
+f = d-e
+print(d,"-",e,"=",f)
+g = int(input("Enter number :"))
+h = int(input("Enter number :"))
+i = g*h
+print(g,"*",h,"=",i)
+j = int(input("Enter number :"))
+k = int(input("Enter number :"))
+l = j/k
+print(j,"/",k,"=",l)
+print("------------------")
+print(a,"+",b,"=",c)
+print(d,"-",e,"=",f)
+print(g,"*",h,"=",i)
+print(j,"/",k,"=",l)
+print("------------------")
---
+++ Exercise5_2_Akwa_B.py
@@ -0,0 +1,8 @@
+distance = float(input("Enter the distance in kilometers: "))
+print(distance,"km")
+time = float(input("Enter the time in hour: "))
+print(time,"h")
+rateOfSpeed = distance/time
+print("_____________________")
+print(rateOfSpeed,"km/h")
+print("_____________________")
